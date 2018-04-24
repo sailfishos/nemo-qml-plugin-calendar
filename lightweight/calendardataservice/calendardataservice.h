sailfishos/nemo-qml-plugin-calendar

commit e56894b32012975c66174dbc81d801e42ac01c36

[nemo-qml-plugin-calendar] Changes for accept invitation from email. Contributes to JB#15386
Changes include:
- Support import event from raw ICS data (UTF-8 encoding)
- Added enum for invitation responses
- Support secrecy for CalendarImportEvent (Public, Private, Confidential)
- Support property for getting value for reminder in seconds for CalendarImportEvent
- Added 'organizer' and 'organizerEmail' properties to CalendarImportEvent

src/calendarimportevent.cpp
@@ -98,6 +98,14 @@ NemoCalendarEvent::Reminder CalendarImportEvent::reminder() const
     return NemoCalendarUtils::getReminder(mEvent);
 }
 
+int CalendarImportEvent::reminderSeconds() const
+{
+    if (!mEvent)
+        return 0;
+
+    return NemoCalendarUtils::getReminderSeconds(mEvent);
+}
+
 QString CalendarImportEvent::uniqueId() const
 {
     if (!mEvent)
@@ -125,6 +133,30 @@ QList<QObject *> CalendarImportEvent::attendees() const
         return QList<QObject *>();
 
     return NemoCalendarUtils::convertAttendeeList(NemoCalendarUtils::getEventAttendees(mEvent));
+}
+
+NemoCalendarEvent::Secrecy CalendarImportEvent::secrecy() const
+{
+    if (!mEvent)
+        return NemoCalendarEvent::SecrecyPublic;
+
+    return NemoCalendarUtils::convertSecrecy(mEvent);
+}
+
+QString CalendarImportEvent::organizer() const
+{
+    if (!mEvent || !mEvent->organizer())
+        return QString();
+
+    return mEvent->organizer()->fullName();
+}
+
+QString CalendarImportEvent::organizerEmail() const
+{
+    if (!mEvent || !mEvent->organizer())
+        return QString();
+
+    return mEvent->organizer()->email();
 }
 
 void CalendarImportEvent::setColor(const QString &color)

src/calendarutils.cpp
@@ -70,7 +70,21 @@ NemoCalendarEvent::Recur NemoCalendarUtils::convertRecurrence(const KCalCore::Ev
     return NemoCalendarEvent::RecurCustom;
 }
 
-NemoCalendarEvent::Reminder NemoCalendarUtils::getReminder(const KCalCore::Event::Ptr &event)
+NemoCalendarEvent::Secrecy NemoCalendarUtils::convertSecrecy(const KCalCore::Event::Ptr &event)
+{
+    KCalCore::Incidence::Secrecy s = event->secrecy();
+    switch (s) {
+    case KCalCore::Incidence::SecrecyPrivate:
+        return NemoCalendarEvent::SecrecyPrivate;
+    case KCalCore::Incidence::SecrecyConfidential:
+        return NemoCalendarEvent::SecrecyConfidential;
+    case KCalCore::Incidence::SecrecyPublic:
+    default:
+        return NemoCalendarEvent::SecrecyPublic;
+    }
+}
+
+int NemoCalendarUtils::getReminderSeconds(const KCalCore::Event::Ptr &event)
 {
     KCalCore::Alarm::List alarms = event->alarms();
 
@@ -90,7 +104,12 @@ NemoCalendarEvent::Reminder NemoCalendarUtils::getReminder(const KCalCore::Event
         return NemoCalendarEvent::ReminderNone;
 
     KCalCore::Duration d = alarm->startOffset();
-    int sec = d.asSeconds();
+    return d.asSeconds();
+}
+
+NemoCalendarEvent::Reminder NemoCalendarUtils::getReminder(const KCalCore::Event::Ptr &event)
+{
+    int sec = getReminderSeconds(event);
 
     switch (sec) {
     case 0:
@@ -226,6 +245,18 @@ bool NemoCalendarUtils::importFromFile(const QString &fileName,
     }
     if (!ok)
         qWarning() << "Failed to import from file" << filePath;
+
+    return ok;
+}
+
+bool NemoCalendarUtils::importFromIcsRawData(const QByteArray &icsData,
+                                             KCalCore::Calendar::Ptr calendar)
+{
+    bool ok = false;
+    KCalCore::ICalFormat icalFormat;
+    ok = icalFormat.fromRawString(calendar, icsData);
+    if (!ok)
+        qWarning() << "Failed to import from raw data";
 
     return ok;
 }

src/calendarutils.h
@@ -43,12 +43,15 @@
 namespace NemoCalendarUtils {
 
 NemoCalendarEvent::Recur convertRecurrence(const KCalCore::Event::Ptr &event);
+NemoCalendarEvent::Secrecy convertSecrecy(const KCalCore::Event::Ptr &event);
+int getReminderSeconds(const KCalCore::Event::Ptr &event);
 NemoCalendarEvent::Reminder getReminder(const KCalCore::Event::Ptr &event);
 QList<NemoCalendarData::Attendee> getEventAttendees(const KCalCore::Event::Ptr &event);
 QList<QObject*> convertAttendeeList(const QList<NemoCalendarData::Attendee> &list);
 NemoCalendarData::EventOccurrence getNextOccurrence(const KCalCore::Event::Ptr &event,
                                                     const QDateTime &start = QDateTime::currentDateTime());
 bool importFromFile(const QString &fileName, KCalCore::Calendar::Ptr calendar);
+bool importFromIcsRawData(const QByteArray &icsData, KCalCore::Calendar::Ptr calendar);
 
 } // namespace NemoCalendarUtils
 

src/calendardata.h
@@ -37,6 +37,7 @@ struct Event {
     KDateTime recurrenceId;
     bool readonly;
     QString location;
+    NemoCalendarEvent::Secrecy secrecy;
     QString calendarUid;
 
     bool operator==(const Event& other) const

src/calendarevent.cpp
@@ -122,6 +122,11 @@ QString NemoCalendarEvent::location() const
     return mManager->getEvent(mUniqueId, mRecurrenceId).location;
 }
 
+NemoCalendarEvent::Secrecy NemoCalendarEvent::secrecy() const
+{
+    return mManager->getEvent(mUniqueId, mRecurrenceId).secrecy;
+}
+
 KDateTime NemoCalendarEvent::recurrenceId() const
 {
     return mRecurrenceId;

src/calendarevent.h
@@ -46,6 +46,8 @@ class NemoCalendarEvent : public QObject
     Q_ENUMS(Recur)
     Q_ENUMS(Reminder)
     Q_ENUMS(TimeSpec)
+    Q_ENUMS(Secrecy)
+    Q_ENUMS(Response)
 
     Q_PROPERTY(QString displayLabel READ displayLabel NOTIFY displayLabelChanged)
     Q_PROPERTY(QString description READ description NOTIFY descriptionChanged)
@@ -62,6 +64,7 @@ class NemoCalendarEvent : public QObject
     Q_PROPERTY(bool readonly READ readonly CONSTANT)
     Q_PROPERTY(QString calendarUid READ calendarUid NOTIFY calendarUidChanged)
     Q_PROPERTY(QString location READ location NOTIFY locationChanged)
+    Q_PROPERTY(Secrecy secrecy READ secrecy NOTIFY secrecyChanged)
 
 public:
     enum Recur {
@@ -91,6 +94,19 @@ public:
         SpecClockTime
     };
 
+    enum Secrecy {
+        SecrecyPublic,
+        SecrecyPrivate,
+        SecrecyConfidential
+    };
+
+    enum Response {
+        ResponseUnspecified,
+        ResponseAccept,
+        ResponseTentative,
+        ResponseDecline
+    };
+
     NemoCalendarEvent(NemoCalendarManager *manager, const QString &uid, const KDateTime &recurrenceId);
     ~NemoCalendarEvent();
 
@@ -110,6 +126,7 @@ public:
     QString location() const;
     KDateTime recurrenceId() const;
     QString recurrenceIdString() const;
+    Secrecy secrecy() const;
 
     Q_INVOKABLE QString vCalendar(const QString &prodId = QString()) const;
 
@@ -131,6 +148,7 @@ signals:
     void locationChanged();
     void recurEndDateChanged();
     void hasRecurEndDateChanged();
+    void secrecyChanged();
 
 private:
     NemoCalendarManager *mManager;

src/calendarimportevent.h
@@ -49,11 +49,15 @@ class CalendarImportEvent : public QObject
     Q_PROPERTY(QDateTime endTime READ endTime CONSTANT)
     Q_PROPERTY(bool allDay READ allDay CONSTANT)
     Q_PROPERTY(NemoCalendarEvent::Recur recur READ recur CONSTANT)
+    Q_PROPERTY(int reminderSeconds READ reminderSeconds CONSTANT)
     Q_PROPERTY(NemoCalendarEvent::Reminder reminder READ reminder CONSTANT)
     Q_PROPERTY(QString uniqueId READ uniqueId CONSTANT)
     Q_PROPERTY(QString color READ color WRITE setColor NOTIFY colorChanged)
     Q_PROPERTY(QString location READ location CONSTANT)
     Q_PROPERTY(QList<QObject*> attendees READ attendees CONSTANT)
+    Q_PROPERTY(QString organizer READ organizer CONSTANT)
+    Q_PROPERTY(QString organizerEmail READ organizerEmail CONSTANT)
+    Q_PROPERTY(NemoCalendarEvent::Secrecy secrecy READ secrecy CONSTANT)
 
 public:
     CalendarImportEvent(KCalCore::Event::Ptr event);
@@ -64,11 +68,15 @@ public:
     QDateTime endTime() const;
     bool allDay() const;
     NemoCalendarEvent::Recur recur();
+    int reminderSeconds() const;
     NemoCalendarEvent::Reminder reminder() const;
     QString uniqueId() const;
     QString color() const;
     QString location() const;
     QList<QObject*> attendees() const;
+    NemoCalendarEvent::Secrecy secrecy() const;
+    QString organizer() const;
+    QString organizerEmail() const;
 
     void setColor(const QString &color);
 

src/calendarimportmodel.cpp
@@ -71,14 +71,23 @@ void NemoCalendarImportModel::setFileName(const QString &fileName)
 
     mFileName = fileName;
     emit fileNameChanged();
-    if (!mFileName.isEmpty()) {
-        importToMemory(fileName);
-    } else if (!mEventList.isEmpty()) {
-        beginResetModel();
-        mEventList.clear();
-        endResetModel();
-        emit countChanged();
-    }
+    reload();
+}
+
+QString NemoCalendarImportModel::icsUtf8RawString() const
+{
+    return QString::fromUtf8(mIcsRawData);
+}
+
+void NemoCalendarImportModel::setIcsUtf8RawString(const QString &icsData)
+{
+    QByteArray data = icsData.toUtf8();
+    if (mIcsRawData == data)
+        return;
+
+    mIcsRawData = data;
+    emit icsUtf8RawStringChanged();
+    reload();
 }
 
 QObject *NemoCalendarImportModel::getEvent(int index)
@@ -184,14 +193,30 @@ static bool incidenceLessThan(const KCalCore::Incidence::Ptr e1,
     }
 }
 
-bool NemoCalendarImportModel::importToMemory(const QString &fileName)
+void NemoCalendarImportModel::reload()
+{
+    if (!mFileName.isEmpty() || !mIcsRawData.isEmpty()) {
+        importToMemory(mFileName, mIcsRawData);
+    } else if (!mEventList.isEmpty()) {
+        beginResetModel();
+        mEventList.clear();
+        endResetModel();
+        emit countChanged();
+    }
+}
+
+bool NemoCalendarImportModel::importToMemory(const QString &fileName, const QByteArray &icsData)
 {
     if (!mEventList.isEmpty())
         mEventList.clear();
 
     beginResetModel();
     KCalCore::MemoryCalendar::Ptr cal(new KCalCore::MemoryCalendar(KDateTime::Spec::LocalZone()));
-    NemoCalendarUtils::importFromFile(fileName, cal);
+    if (!fileName.isEmpty()) {
+        NemoCalendarUtils::importFromFile(fileName, cal);
+    } else {
+        NemoCalendarUtils::importFromIcsRawData(icsData, cal);
+    }
     KCalCore::Incidence::List incidenceList = cal->incidences();
     for (int i = 0; i < incidenceList.size(); i++) {
         KCalCore::Incidence::Ptr incidence = incidenceList.at(i);

commit 537ce11ff765c4a6c043a243945e4f2b12eb3e04

[calendar-plugin] Fix saving event with no reminder. Fixes JB#41760
Commit e56894b32012975c66 broke getReminder(), was returning
NemoCalendarEvent::ReminderNone => 0 for no reminder which also
means at the start of the event.

lightweight/calendardataservice/calendardataservice.h
@@ -41,7 +41,8 @@
 
 class NemoCalendarAgendaModel;
 
-class CalendarDataService : public QObject {
+class CalendarDataService : public QObject
+{
     Q_OBJECT
 
 public:

src/calendarimportevent.cpp
@@ -100,10 +100,12 @@ NemoCalendarEvent::Reminder CalendarImportEvent::reminder() const
 
 int CalendarImportEvent::reminderSeconds() const
 {
+    // FIXME: this doesn't know how to say "no reminder".
     if (!mEvent)
         return 0;
 
-    return NemoCalendarUtils::getReminderSeconds(mEvent);
+    bool hasReminder = false;
+    return NemoCalendarUtils::getReminderSeconds(mEvent, &hasReminder);
 }
 
 QString CalendarImportEvent::uniqueId() const

src/calendarutils.cpp
@@ -84,7 +84,7 @@ NemoCalendarEvent::Secrecy NemoCalendarUtils::convertSecrecy(const KCalCore::Eve
     }
 }
 
-int NemoCalendarUtils::getReminderSeconds(const KCalCore::Event::Ptr &event)
+int NemoCalendarUtils::getReminderSeconds(const KCalCore::Event::Ptr &event, bool *hasReminder)
 {
     KCalCore::Alarm::List alarms = event->alarms();
 
@@ -94,22 +94,32 @@ int NemoCalendarUtils::getReminderSeconds(const KCalCore::Event::Ptr &event)
         if (alarms.at(ii)->type() == KCalCore::Alarm::Procedure)
             continue;
 
-        if (alarm)
-            return NemoCalendarEvent::ReminderNone;
-        else
+        if (alarm) {
+            *hasReminder = false;
+            return 0;
+        } else {
             alarm = alarms.at(ii);
+        }
     }
 
-    if (!alarm)
-        return NemoCalendarEvent::ReminderNone;
+    if (!alarm) {
+        *hasReminder = false;
+        return 0;
+    }
 
     KCalCore::Duration d = alarm->startOffset();
+    *hasReminder = true;
     return d.asSeconds();
 }
 
 NemoCalendarEvent::Reminder NemoCalendarUtils::getReminder(const KCalCore::Event::Ptr &event)
 {
-    int sec = getReminderSeconds(event);
+    bool hasReminder = false;
+    int sec = getReminderSeconds(event, &hasReminder);
+
+    if (!hasReminder) {
+        return NemoCalendarEvent::ReminderNone;
+    }
 
     switch (sec) {
     case 0:

src/calendarutils.h
@@ -44,7 +44,7 @@ namespace NemoCalendarUtils {
 
 NemoCalendarEvent::Recur convertRecurrence(const KCalCore::Event::Ptr &event);
 NemoCalendarEvent::Secrecy convertSecrecy(const KCalCore::Event::Ptr &event);
-int getReminderSeconds(const KCalCore::Event::Ptr &event);
+int getReminderSeconds(const KCalCore::Event::Ptr &event, bool *hasReminder);
 NemoCalendarEvent::Reminder getReminder(const KCalCore::Event::Ptr &event);
 QList<NemoCalendarData::Attendee> getEventAttendees(const KCalCore::Event::Ptr &event);
 QList<QObject*> convertAttendeeList(const QList<NemoCalendarData::Attendee> &list);