4f8c1ce43f19f6b8c04dca29f5a79d8db3249bf2
Damien Caliste <dcaliste@free.fr>
2020-07-17 17:25:20 +0200

    [nemo-qml-plugin-calendar] Expose sync failures to events. Contributes to JB#50817

--- src/calendarevent.h
+++ src/calendarevent.h
@@ -65,6 +65,7 @@ class CalendarEvent : public QObject
     Q_PROPERTY(QString calendarUid READ calendarUid NOTIFY calendarUidChanged)
     Q_PROPERTY(QString location READ location NOTIFY locationChanged)
     Q_PROPERTY(CalendarEvent::Secrecy secrecy READ secrecy NOTIFY secrecyChanged)
+    Q_PROPERTY(CalendarEvent::SyncFailure syncFailure READ syncFailure NOTIFY syncFailureChanged)
     Q_PROPERTY(CalendarEvent::Response ownerStatus READ ownerStatus NOTIFY ownerStatusChanged)
     Q_PROPERTY(bool rsvp READ rsvp NOTIFY rsvpChanged)
     Q_PROPERTY(bool externalInvitation READ externalInvitation NOTIFY externalInvitationChanged)
@@ -115,6 +116,14 @@ public:
         ResponseDecline
     };
 
+    enum SyncFailure {
+        NoSyncFailure,
+        UploadFailure,
+        UpdateFailure,
+        DeleteFailure
+    };
+    Q_ENUM(SyncFailure)
+
     CalendarEvent(CalendarManager *manager, const QString &uid, const KDateTime &recurrenceId);
     ~CalendarEvent();
 
@@ -136,6 +145,7 @@ public:
     KDateTime recurrenceId() const;
     QString recurrenceIdString() const;
     Secrecy secrecy() const;
+    SyncFailure syncFailure() const;
     Response ownerStatus() const;
     bool rsvp() const;
     bool externalInvitation() const;
@@ -163,6 +173,7 @@ signals:
     void hasRecurEndDateChanged();
     void recurWeeklyDaysChanged();
     void secrecyChanged();
+    void syncFailureChanged();
     void ownerStatusChanged();
     void rsvpChanged();
     void externalInvitationChanged();
