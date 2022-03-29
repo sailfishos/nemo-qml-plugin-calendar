sailfishos/nemo-qml-plugin-calendar

commit ca935edb337eb7ced42773f5be1fa3a2c4a5e9d2

[nemo-qml-plugin-calendar] Don't reload from DB when not needed.

src/calendarutils.cpp
@@ -356,30 +356,34 @@ QList<CalendarData::Attendee> CalendarUtils::getEventAttendees(const KCalendarCo
     QList<CalendarData::Attendee> result;
     const KCalendarCore::Person calOrganizer = event->organizer();
 
-    CalendarData::Attendee organizer;
-    if (!calOrganizer.email().isEmpty()) {
+    const KCalendarCore::Attendee::List attendees = event->attendees();
+    CalendarData::Attendee attendee;
+    for (const KCalendarCore::Attendee &calAttendee : attendees) {
+        attendee.name = calAttendee.name();
+        attendee.email = calAttendee.email();
+        attendee.isOrganizer = (attendee.email == calOrganizer.email());
+        if (attendee.isOrganizer) {
+            // If organizer is in the attendee list, we prioritize the
+            // details from the attendee structure.
+            attendee.participationRole = KCalendarCore::Attendee::Chair;
+            result.prepend(attendee);
+        } else {
+            attendee.status = calAttendee.status();
+            attendee.participationRole = calAttendee.role();
+            result.append(attendee);
+        }
+    }
+
+    if (result.isEmpty() || !result.first().isOrganizer) {
+        // We always prepend the organizer in the list returned to QML.
+        // If it was not present in the attendee list, we create one attendee
+        // from the data in the ::Person structure.
+        CalendarData::Attendee organizer;
         organizer.isOrganizer = true;
         organizer.name = calOrganizer.name();
         organizer.email = calOrganizer.email();
         organizer.participationRole = KCalendarCore::Attendee::Chair;
-        result.append(organizer);
-    }
-
-    const KCalendarCore::Attendee::List attendees = event->attendees();
-    CalendarData::Attendee attendee;
-    attendee.isOrganizer = false;
-
-    for (const KCalendarCore::Attendee &calAttendee : attendees) {
-        attendee.name = calAttendee.name();
-        attendee.email = calAttendee.email();
-        if (attendee.name == organizer.name && attendee.email == organizer.email) {
-            // avoid duplicate info
-            continue;
-        }
-
-        attendee.status = calAttendee.status();
-        attendee.participationRole = calAttendee.role();
-        result.append(attendee);
+        result.prepend(organizer);
     }
 
     return result;

tests/tst_calendarevent/tst_calendarevent.cpp
@@ -192,7 +192,13 @@ void tst_CalendarEvent::testSave()
     eventMod->setLocation(location);
     QCOMPARE(eventMod->location(), location);
 
-    QDateTime endTime = QDateTime::currentDateTime();
+    QDateTime startTime = QDateTime::currentDateTime().addDays(-200);
+    const QTime at = startTime.time();
+    startTime.setTime(QTime(at.hour(), at.minute(), at.second()));
+    eventMod->setStartTime(startTime, Qt::LocalTime);
+    QCOMPARE(eventMod->startTime(), startTime);
+
+    QDateTime endTime = startTime.addSecs(3600);
     eventMod->setEndTime(endTime, Qt::LocalTime);
     QCOMPARE(eventMod->endTime(), endTime);
 
@@ -207,10 +213,6 @@ void tst_CalendarEvent::testSave()
     int reminder = 0; // at the time of the event
     eventMod->setReminder(reminder);
     QCOMPARE(eventMod->reminder(), reminder);
-
-    QDateTime startTime = QDateTime::currentDateTime();
-    eventMod->setStartTime(startTime, Qt::LocalTime);
-    QCOMPARE(eventMod->startTime(), startTime);
 
     QString uid;
     bool ok = saveEvent(eventMod, &uid);
@@ -478,12 +480,11 @@ void tst_CalendarEvent::testRecurrenceException()
     }
     QVERIFY(!modificationFound);
 
-    // ensure all gone, this emits two warning for not finding the two occurrences.
+    // ensure all is gone
     calendarApi->removeAll(uid);
-    QVERIFY(dataUpdated.wait());
+    QVERIFY(updated.wait());
+    QVERIFY(!query.event());
     mSavedEvents.remove(uid);
-    QVERIFY(!CalendarManager::instance()->getEvent(uid, QDateTime()).isValid());
-    QVERIFY(!CalendarManager::instance()->getEvent(uid, QDateTime::fromString(recurrenceException->recurrenceIdString(), Qt::ISODate)).isValid());
 
     delete recurrenceException;
     delete mod;

rpm/nemo-qml-plugin-calendar-qt5.spec
@@ -10,7 +10,7 @@ BuildRequires:  pkgconfig(Qt5Core)
 BuildRequires:  pkgconfig(Qt5Gui)
 BuildRequires:  pkgconfig(Qt5Qml)
 BuildRequires:  pkgconfig(Qt5Concurrent)
-BuildRequires:  pkgconfig(libmkcal-qt5) >= 0.5.39
+BuildRequires:  pkgconfig(libmkcal-qt5) >= 0.6.0
 BuildRequires:  pkgconfig(KF5CalendarCore)
 BuildRequires:  pkgconfig(accounts-qt5)
 

src/calendarmanager.cpp
@@ -601,9 +601,8 @@ void CalendarManager::findMatchingEventFinished(const QString &invitationFile, c
     }
 }
 
-void CalendarManager::storageModifiedSlot(const QString &info)
+void CalendarManager::storageModifiedSlot()
 {
-    Q_UNUSED(info);
     mResetPending = true;
     emit storageModified();
 }

src/calendarmanager.h
@@ -112,7 +112,7 @@ public:
     QList<CalendarData::Attendee> getEventAttendees(const QString &uid, const QDateTime &recurrenceId, bool *resultValid);
 
 private slots:
-    void storageModifiedSlot(const QString &info);
+    void storageModifiedSlot();
     void eventNotebookChanged(const QString &oldEventUid, const QString &newEventUid, const QString &notebookUid);
     void excludedNotebooksChangedSlot(const QStringList &excludedNotebooks);
     void notebooksChangedSlot(const QList<CalendarData::Notebook> &notebooks);

src/calendarworker.cpp
@@ -72,7 +72,7 @@ namespace {
 }
 
 CalendarWorker::CalendarWorker()
-    : QObject(0), mAccountManager(0), mHasRecurringEvents(false)
+    : QObject(0), mAccountManager(0)
 {
 }
 
@@ -87,32 +87,28 @@ CalendarWorker::~CalendarWorker()
 
 void CalendarWorker::storageModified(mKCal::ExtendedStorage *storage, const QString &info)
 {
-    Q_UNUSED(storage)
-    Q_UNUSED(info)
+    Q_UNUSED(storage);
+    Q_UNUSED(info);
 
-    // 'info' is either a path to the database (in which case we're screwed, we
-    // have no idea what changed, so tell all interested models to reload) or a
-    // space-seperated list of event UIDs.
-    //
-    // unfortunately we don't know *what* about these events changed with the
-    // current mkcal API, so we'll have to try our best to guess when the time
-    // comes.
-    mSentEvents.clear();
+    // External touch of the database. We have no clue what changed.
+    // The mCalendar content has been wiped out already.
+    mLoadedRanges.clear();
     loadNotebooks();
-    emit storageModifiedSignal(info);
+    emit storageModifiedSignal();
 }
 
-void CalendarWorker::storageProgress(mKCal::ExtendedStorage *storage, const QString &info)
+void CalendarWorker::storageUpdated(mKCal::ExtendedStorage *storage,
+                                    const KCalendarCore::Incidence::List &added,
+                                    const KCalendarCore::Incidence::List &modified,
+                                    const KCalendarCore::Incidence::List &deleted)
 {
-    Q_UNUSED(storage)
-    Q_UNUSED(info)
-}
+    Q_UNUSED(storage);
+    Q_UNUSED(added);
+    Q_UNUSED(modified);
+    Q_UNUSED(deleted);
 
-void CalendarWorker::storageFinished(mKCal::ExtendedStorage *storage, bool error, const QString &info)
-{
-    Q_UNUSED(storage)
-    Q_UNUSED(error)
-    Q_UNUSED(info)
+    // No smart handling of known updates at the moment.
+    emit storageModifiedSignal();
 }
 
 void CalendarWorker::deleteEvent(const QString &uid, const QDateTime &recurrenceId, const QDateTime &dateTime)
@@ -508,8 +504,9 @@ void CalendarWorker::setDefaultNotebook(const QString &notebookUid)
     if (mStorage->defaultNotebook() && mStorage->defaultNotebook()->uid() == notebookUid)
         return;
 
-    mStorage->setDefaultNotebook(mStorage->notebook(notebookUid));
-    mStorage->save();
+    if (!mStorage->setDefaultNotebook(mStorage->notebook(notebookUid))) {
+        qWarning() << "unable to set default notebook";
+    }
 }
 
 QStringList CalendarWorker::excludedNotebooks() const
@@ -642,20 +639,23 @@ void CalendarWorker::loadData(const QList<CalendarData::Range> &ranges,
                               const QStringList &instanceList,
                               bool reset)
 {
-    if (reset)
-        mHasRecurringEvents = false;
-
-    foreach (const CalendarData::Range &range, ranges)
-        mStorage->load(range.first, range.second.addDays(1)); // end date is not inclusive
-
-    foreach (const QString &id, instanceList)
-        mStorage->loadIncidenceInstance(id);
-
-    if (!ranges.isEmpty() && !mHasRecurringEvents) {
+    if (!ranges.isEmpty() && mLoadedRanges.isEmpty()) {
         // Load all recurring incidences,
         // we have no other way to detect if they occur within a range
         mStorage->loadRecurringIncidences();
-        mHasRecurringEvents = true;
+    }
+
+    for (const CalendarData::Range &range : ranges) {
+        if (!mLoadedRanges.contains(range)) {
+            mStorage->load(range.first, range.second.addDays(1)); // end date is not inclusive
+            mLoadedRanges.insert(range);
+        }
+    }
+
+    foreach (const QString &id, instanceList) {
+        if (mCalendar->instance(id).isNull()) {
+            mStorage->loadIncidenceInstance(id);
+        }
     }
 
     if (reset)
@@ -694,11 +694,11 @@ void CalendarWorker::loadData(const QList<CalendarData::Range> &ranges,
             continue;
         }
 
-        if (!mSentEvents.contains(e->uid(), e->recurrenceId())) {
+        const QString id = e->instanceIdentifier();
+        if (!mSentEvents.contains(id)) {
             CalendarData::Event event = createEventStruct(e, notebook);
-            mSentEvents.insert(event.uniqueId, event.recurrenceId);
+            mSentEvents.insert(id);
             events.insert(event.uniqueId, event);
-            const QString id = e->instanceIdentifier();
             if (id != event.uniqueId) {
                 // Ensures that events can also be retrieved by instanceIdentifier
                 events.insert(id, event);

src/calendarworker.h
@@ -56,8 +56,10 @@ public:
 
     /* mKCal::ExtendedStorageObserver */
     void storageModified(mKCal::ExtendedStorage *storage, const QString &info);
-    void storageProgress(mKCal::ExtendedStorage *storage, const QString &info);
-    void storageFinished(mKCal::ExtendedStorage *storage, bool error, const QString &info);
+    void storageUpdated(mKCal::ExtendedStorage *storage,
+                        const KCalendarCore::Incidence::List &added,
+                        const KCalendarCore::Incidence::List &modified,
+                        const KCalendarCore::Incidence::List &deleted);
 
 public slots:
     void init();
@@ -88,7 +90,7 @@ public slots:
     void findMatchingEvent(const QString &invitationFile);
 
 signals:
-    void storageModifiedSignal(const QString &info);
+    void storageModifiedSignal();
 
     void eventNotebookChanged(const QString &oldEventUid, const QString &newEventUid, const QString &notebookUid);
 
@@ -137,10 +139,9 @@ private:
 
     QHash<QString, CalendarData::Notebook> mNotebooks;
 
-    // Tracks which events have been already passed to manager. Maps Uid -> RecurrenceId
-    QMultiHash<QString, QDateTime> mSentEvents;
-
-    bool mHasRecurringEvents;
+    // Tracks which events have been already passed to manager, using instanceIdentifiers.
+    QSet<QString> mSentEvents;
+    QSet<CalendarData::Range> mLoadedRanges;
 };
 
 #endif // CALENDARWORKER_H

tests/tst_calendarmanager/tst_calendarmanager.cpp
@@ -586,13 +586,9 @@ void tst_CalendarManager::test_notebookApi()
         QVERIFY(uidList.contains(notebookPtr->uid()));
 
     QSignalSpy defaultNotebookSpy(mManager, SIGNAL(defaultNotebookChanged(QString)));
+    notebookSpy.clear();
     mManager->setDefaultNotebook(mAddedNotebooks.first()->uid());
-    for (int i = 0; i < 30; i++) {
-        if (notebookSpy.count() > 3)
-            break;
-
-        QTest::qWait(100);
-    }
+    QVERIFY(notebookSpy.wait());
     QCOMPARE(mManager->defaultNotebook(), mAddedNotebooks.first()->uid());
     QCOMPARE(defaultNotebookSpy.count(), 1);
 

commit 504569b94d16fb001bbedb52e31ca63d2df93e70

[nemo-qml-plugin-calendar] Fix empty organizer.
Fix a regression introduced in ca935edb where
an organizer is always added to the attendee list
even when empty.

src/calendarutils.cpp
@@ -361,7 +361,8 @@ QList<CalendarData::Attendee> CalendarUtils::getEventAttendees(const KCalendarCo
     for (const KCalendarCore::Attendee &calAttendee : attendees) {
         attendee.name = calAttendee.name();
         attendee.email = calAttendee.email();
-        attendee.isOrganizer = (attendee.email == calOrganizer.email());
+        attendee.isOrganizer = (!calOrganizer.email().isEmpty()
+                                && attendee.email == calOrganizer.email());
         if (attendee.isOrganizer) {
             // If organizer is in the attendee list, we prioritize the
             // details from the attendee structure.
@@ -374,7 +375,8 @@ QList<CalendarData::Attendee> CalendarUtils::getEventAttendees(const KCalendarCo
         }
     }
 
-    if (result.isEmpty() || !result.first().isOrganizer) {
+    if (!calOrganizer.email().isEmpty()
+        && (result.isEmpty() || !result.first().isOrganizer)) {
         // We always prepend the organizer in the list returned to QML.
         // If it was not present in the attendee list, we create one attendee
         // from the data in the ::Person structure.

tests/tst_calendarevent/tst_calendarevent.cpp
@@ -705,16 +705,42 @@ void tst_CalendarEvent::testAttendees()
     defaultNotebook->setCustomProperty("TEST_EMAIL", QString::fromLatin1("alice@example.org"));
     QVERIFY(storage->updateNotebook(defaultNotebook));
 
+    // Test first the case without attendee.
+    CalendarEventModification *eventMod = calendarApi->createNewEvent();
+    QVERIFY(eventMod != 0);
+
+    eventMod->setStartTime(QDateTime(QDate(2022, 3, 29), QTime(11, 7)), Qt::LocalTime);
+    eventMod->setEndTime(eventMod->startTime().addSecs(600), Qt::LocalTime);
+    eventMod->setDescription(QString::fromLatin1("Test without attendee"));
+
+    QString uid;
+    bool ok = saveEvent(eventMod, &uid);
+    if (!ok) {
+        QFAIL("Failed to fetch new event uid");
+    }
+    QVERIFY(!uid.isEmpty());
+    mSavedEvents.insert(uid);
+    delete eventMod;
+
+    CalendarEventQuery query;
+    QSignalSpy eventSpy(&query, &CalendarEventQuery::attendeesChanged);
+    query.setUniqueId(uid);
+    QVERIFY(!eventSpy.wait(250));
+
+    QVERIFY(query.attendees().isEmpty());
+
+    // Test the case with attendees
     TestInvitationPlugin *plugin = static_cast<TestInvitationPlugin*>(mKCal::ServiceHandler::instance().service(defaultNotebook->pluginName()));
     QVERIFY(plugin);
+    QVERIFY(!plugin->sentInvitation());
 
-    CalendarEventModification *eventMod = calendarApi->createNewEvent();
+    eventMod = calendarApi->createNewEvent();
     QVERIFY(eventMod != 0);
 
     eventMod->setStartTime(QDateTime::currentDateTime(), Qt::LocalTime);
     eventMod->setEndTime(eventMod->startTime().addSecs(600), Qt::LocalTime);
     const QString initialDescr = QString::fromLatin1("Test attendees");
-    eventMod->setDisplayLabel(initialDescr);
+    eventMod->setDescription(initialDescr);
     CalendarContactModel required, optional;
     const QString alice = QString::fromLatin1("Alice");
     const QString bob = QString::fromLatin1("Bob");
@@ -730,8 +756,7 @@ void tst_CalendarEvent::testAttendees()
     Person Bob(bob, bobEmail, false, Person::RequiredParticipant, Person::UnknownParticipation);
     Person Carl(carl, carlEmail, false, Person::OptionalParticipant, Person::UnknownParticipation);
 
-    QString uid;
-    bool ok = saveEvent(eventMod, &uid);
+    ok = saveEvent(eventMod, &uid);
     if (!ok) {
         QFAIL("Failed to fetch new event uid");
     }
@@ -739,7 +764,7 @@ void tst_CalendarEvent::testAttendees()
     mSavedEvents.insert(uid);
     delete eventMod;
 
-    // Check that the sendInvitation() service as received the right data.
+    // Check that the sendInvitation() service has received the right data.
     const KCalendarCore::Incidence::Ptr sentInvitation = plugin->sentInvitation();
     QVERIFY(sentInvitation);
     QCOMPARE(sentInvitation->uid(), uid);
@@ -753,8 +778,6 @@ void tst_CalendarEvent::testAttendees()
     QVERIFY(sentAttendees.contains(attCarl));
 
     // Check that saved event locally is presenting the right data.
-    CalendarEventQuery query;
-    QSignalSpy eventSpy(&query, &CalendarEventQuery::attendeesChanged);
     query.setUniqueId(uid);
     QVERIFY(eventSpy.wait());
 